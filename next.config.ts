316393a48bf64d0092747efa2952c4b352fafdcf
saurav_7709 <shailendrakumarsingh13316@gmail.com>
2025-10-28 23:05:15 +0530

    chore(prisma): add @prisma/nextjs-monorepo-workaround-plugin to dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ['lh3.googleusercontent.com'], // add any other domains you need
+  },
 };
 
 export default nextConfig;
